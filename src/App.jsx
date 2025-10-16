97110665115f13e57e73bb52e2b046521e223c02
Hien <s4073525@rmit.edu.vn>
2025-10-16 18:01:56 +0700

    Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import HomePage from './pages/HomePage'
+import ErrorPage from './pages/ErrorPage'
+import RenewablePage from './pages/RenewablePage'
 
 function App() {
   return (
     <>
+    <Header></Header>
+      <Routes>
+        <Route path='/' element={<HomePage />}></Route>
+        <Route path='/renew' element={<RenewablePage />}></Route>
+
+        <Route path='*' element={<ErrorPage />}></Route>
+      </Routes>
+      <Footer></Footer>
     </>
   )
 }
